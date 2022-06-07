97b90e53763f9b18ce87b7861f5406f529717570
Slacknwork <tphcmtrung@gmail.com>
2022-06-07 10:31:17 +0700

    edit paymentfacade

--- localbrand/src/main/java/com/localbrand/controller/web/HomeController.java
+++ localbrand/src/main/java/com/localbrand/controller/web/HomeController.java
@@ -12,13 +12,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.localbrand.entities.MembershipTier;
+import com.localbrand.entities.Payment;
 import com.localbrand.sessionbeans.MembershipTierFacade;
+import com.localbrand.sessionbeans.PaymentFacade;
 
 /**
  * Servlet implementation class HomeController
  */
 @WebServlet(urlPatterns="/web/home")
 public class HomeController extends HttpServlet {
+	PaymentFacade pf = new PaymentFacade();
 	MembershipTierFacade mstf = new MembershipTierFacade();
 	private static final long serialVersionUID = 1L;
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -39,16 +42,32 @@ public class HomeController extends HttpServlet {
     }
     private void index(HttpServletRequest request, HttpServletResponse response) {
     	List<MembershipTier> list = new ArrayList<>();
+    	List<Payment> listp = new ArrayList<>();
+    	Payment p = new Payment();
+    	int count=0;
+    	int id = 3;
+    	int[] range = new int[]{1,2};
     	try {
+//    		p.setId(4);
+//    		p.setPayMethod("Paypal");
+//    		p.setStatus("0");
+//    		pf.edit(p);
+    		pf.remove(id);
+    		p = pf.find(id);
 			list = mstf.findAll();
+			listp.add(p);
+			count = pf.count();
 		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	System.out.println("vao roi 3"+list.toString());
-
         String hello = "Hai Dep Chai.";
         request.setAttribute("listMembershipTier", list);
-
+        request.setAttribute("listPayment", listp);
+        request.setAttribute("clistPayment", count);
         request.setAttribute("hello", hello);
     }
     
--- localbrand/src/main/webapp/WEB-INF/views/web/home/index.jsp
+++ localbrand/src/main/webapp/WEB-INF/views/web/home/index.jsp
@@ -2,7 +2,11 @@
 	pageEncoding="UTF-8"%>
 <%@ include file="/common/taglib.jsp"%>
 <h1>${hello}</h1>
-Test query membershipTier<br>
+<h1>${clistPayment}</h1>
+Test query Payment<br>
 <c:forEach var="item" items="${listMembershipTier}">
 	${item}<br>
+</c:forEach>
+<c:forEach var="item1" items="${listPayment}">
+	${item1}<br>
 </c:forEach>
