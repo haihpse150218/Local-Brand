1d1c1eb884a9ccb7c67948b033f47f352a708f81
Ngoc Toan <ksgalaxyy@gmail.com>
2022-06-21 17:24:32 +0700

    Added getBrandList to HomeService

---
+++ localbrand/src/main/java/com/localbrand/service/IHome.java
@@ -0,0 +1,15 @@
+package com.localbrand.service;
+
+import java.util.List;
+
+import com.localbrand.entities.Brand;
+import com.localbrand.entities.Collection;
+import com.localbrand.entities.Product;
+
+public interface IHome {
+	public List<Brand> getBrandList(int start, int end) throws Exception;
+	
+	public List<Collection> getTrendingCollection();
+	
+	public List<Product> getTrendingProducts();
+}
