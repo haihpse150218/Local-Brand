b4c8183d5e1a2275b8a62746194360faad907a41
tkiet1808 <kietpttse161220@fpt.edu.vn>
2022-07-20 15:37:53 +0700

    View Cart + Check Out service

--- localbrand/src/main/java/com/localbrand/sessionbeans/OrderFacade.java
+++ localbrand/src/main/java/com/localbrand/sessionbeans/OrderFacade.java
@@ -18,75 +18,73 @@ public class OrderFacade extends AbstractFacade<Order> {
 
 	@Override
 	protected void create(Connection con, Order t) throws SQLException {
-		String sql = "  INSERT INTO [Order] ([OrderDate], [Total], [Tax], [PayId], [CustomerId], [Status])\r\n" + 
-				"values	(?, ?, ?, ?, ?, ?)";
-		
+		String sql = "  INSERT INTO [Order] ([OrderDate], [Total], [Tax], [PayId], [CustomerId], [Status])\r\n"
+				+ "values	(?, ?, ?, ?, ?, ?)";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
-		ptm.setString(1,(t.getOrderDate().getYear() + "-" + t.getOrderDate().getMonth() + "-" + t.getOrderDate().getDate()
-			+ " " + t.getOrderDate().getHours() + ":" + t.getOrderDate().getMinutes() + ":" + t.getOrderDate().getSeconds()));
+		// ptm.setString(1,(t.getOrderDate().getYear() + "-" +
+		// t.getOrderDate().getMonth() + "-" + t.getOrderDate().getDate()
+		// + " " + t.getOrderDate().getHours() + ":" + t.getOrderDate().getMinutes() +
+		// ":" + t.getOrderDate().getSeconds()));
+		java.sql.Date sqlOrderDate = new java.sql.Date(t.getOrderDate().getTime());
+		ptm.setDate(1, sqlOrderDate);
 		ptm.setDouble(2, t.getTotal());
 		ptm.setDouble(3, t.getTax());
 		ptm.setInt(4, t.getPayId().getId());
 		ptm.setInt(5, t.getCustomerId().getId());
 		ptm.setString(6, t.getStatus());
 		ptm.executeUpdate();
-		
+
 	}
 
 	@Override
 	protected void edit(Connection con, Order t) throws SQLException {
-		String sql = "UPDATE [Order] \r\n" + 
-				"SET [OrderDate] = ?\r\n" + 
-				", [Total] = ?\r\n" + 
-				", [Tax] = ?\r\n" + 
-				", [PayId] = ?\r\n" + 
-				", [CustomerId] = ?\r\n" + 
-				", [Status] = ?\r\n" + 
-				"WHERE [Id] = ?";
-		
+		String sql = "UPDATE [Order] \r\n" + "SET [OrderDate] = ?\r\n" + ", [Total] = ?\r\n" + ", [Tax] = ?\r\n"
+				+ ", [PayId] = ?\r\n" + ", [CustomerId] = ?\r\n" + ", [Status] = ?\r\n" + "WHERE [Id] = ?";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
 
-		ptm.setString(1, 
-				(t.getOrderDate().getYear() + "-" + t.getOrderDate().getMonth() + "-" + t.getOrderDate().getDate()
-				+ " " + t.getOrderDate().getHours() + ":" + t.getOrderDate().getMinutes() + ":" + t.getOrderDate().getSeconds()));
+		// ptm.setString(1,(t.getOrderDate().getYear() + "-" +
+		// t.getOrderDate().getMonth() + "-" + t.getOrderDate().getDate()
+		// + " " + t.getOrderDate().getHours() + ":" + t.getOrderDate().getMinutes() +
+		// ":" + t.getOrderDate().getSeconds()));
+		java.sql.Date sqlOrderDate = new java.sql.Date(t.getOrderDate().getTime());
+		ptm.setDate(1, sqlOrderDate);
 		ptm.setDouble(2, t.getTotal());
 		ptm.setDouble(3, t.getTax());
 		ptm.setInt(4, t.getPayId().getId());
 		ptm.setInt(5, t.getCustomerId().getId());
 		ptm.setString(6, t.getStatus());
 		ptm.setInt(7, t.getId());
-		
-		
+
 		ptm.executeUpdate();
-		
+
 	}
 
 	@Override
 	protected void remove(Connection con, Object id) throws SQLException {
-		String sql = "DELETE FROM [Order]"
-				+ " WHERE [Id] = ?";
-	
+		String sql = "DELETE FROM [Order]" + " WHERE [Id] = ?";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
 		ptm.setInt(1, Integer.parseInt(id.toString()));
 		ptm.executeUpdate();
-		
+
 	}
 
 	@Override
 	protected Order find(Connection con, Object id) throws SQLException {
 		Order order = null;
-		
-		String sql = "SELECT * FROM [Order]\r\n" + 
-				"  WHERE [Id] = ?";
-		
+
+		String sql = "SELECT * FROM [Order]\r\n" + "  WHERE [Id] = ?";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
 		ptm.setInt(1, Integer.parseInt(id.toString()));
 		ResultSet rs = ptm.executeQuery();
-		
+
 		if (rs.next()) {
 			order = new Order();
 			order.setId(rs.getInt("Id"));
-			
+
 			String dateTime = rs.getString("OrderDate");
 			SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			try {
@@ -98,21 +96,21 @@ public class OrderFacade extends AbstractFacade<Order> {
 
 			order.setTotal(rs.getDouble("Total"));
 			order.setTax(rs.getDouble("Tax"));
-			
-			//FK
+
+			// FK
 			Payment pm = new Payment();
 			pm.setId(rs.getInt("PayId"));
 			order.setPayId(pm);
-			
-			//FK
+
+			// FK
 			Customer cm = new Customer();
 			cm.setId(rs.getInt("CustomerId"));
 			order.setCustomerId(cm);
-			
+
 			order.setStatus(rs.getString("Status"));
-			
+
 		}
-		
+
 		return order;
 	}
 
@@ -120,14 +118,14 @@ public class OrderFacade extends AbstractFacade<Order> {
 	protected List<Order> findAll(Connection con) throws SQLException {
 		List<Order> list = new ArrayList<>();
 		String sql = "SELECT * FROM [Order]";
-		
+
 		Statement stm = con.createStatement();
 		ResultSet rs = stm.executeQuery(sql);
-		
+
 		while (rs.next()) {
 			Order order = new Order();
 			order.setId(rs.getInt("Id"));
-			
+
 			String dateTime = rs.getString("OrderDate");
 			SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			try {
@@ -139,40 +137,39 @@ public class OrderFacade extends AbstractFacade<Order> {
 
 			order.setTotal(rs.getDouble("Total"));
 			order.setTax(rs.getDouble("Tax"));
-			
-			//FK
+
+			// FK
 			Payment pm = new Payment();
 			pm.setId(rs.getInt("PayId"));
 			order.setPayId(pm);
-			
-			//FK
+
+			// FK
 			Customer cm = new Customer();
 			cm.setId(rs.getInt("CustomerId"));
 			order.setCustomerId(cm);
 			order.setStatus(rs.getString("Status"));
-			
+
 			list.add(order);
 		}
-		
+
 		return list;
 	}
 
 	@Override
 	protected List<Order> findRange(Connection con, int begin, int end) throws SQLException {
 		List<Order> list = new ArrayList<>();
-		
-		String sql = "  SELECT TOP(?) * FROM [Order] \r\n" + 
-				"  EXCEPT SELECT TOP(?) * FROM [Order]";
-		
+
+		String sql = "  SELECT TOP(?) * FROM [Order] \r\n" + "  EXCEPT SELECT TOP(?) * FROM [Order]";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
 		ptm.setInt(1, end);
-		ptm.setInt(2, begin-1);
+		ptm.setInt(2, begin - 1);
 		ResultSet rs = ptm.executeQuery();
-		
+
 		while (rs.next()) {
 			Order order = new Order();
 			order.setId(rs.getInt("Id"));
-			
+
 			String dateTime = rs.getString("OrderDate");
 			SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			try {
@@ -184,42 +181,38 @@ public class OrderFacade extends AbstractFacade<Order> {
 
 			order.setTotal(rs.getDouble("Total"));
 			order.setTax(rs.getDouble("Tax"));
-			
-			//FK
+
+			// FK
 			Payment pm = new Payment();
 			pm.setId(rs.getInt("PayId"));
 			order.setPayId(pm);
-			
-			//FK
+
+			// FK
 			Customer cm = new Customer();
 			cm.setId(rs.getInt("CustomerId"));
 			order.setCustomerId(cm);
 			order.setStatus(rs.getString("Status"));
-			
+
 			list.add(order);
 		}
-		
+
 		return list;
 	}
 
 	@Override
 	protected int count(Connection con) throws SQLException {
 		int result = 0;
-		
-		String sql = "SELECT COUNT([Id])" +
-					" AS [Result]" +
-					" FROM [Order]";
-		
+
+		String sql = "SELECT COUNT([Id])" + " AS [Result]" + " FROM [Order]";
+
 		PreparedStatement ptm = con.prepareStatement(sql);
 		ResultSet rs = ptm.executeQuery();
-		
+
 		if (rs.next()) {
 			result = rs.getInt("Result");
 		}
-		
-		return result;	
+
+		return result;
 	}
 
-   
-    
 }
